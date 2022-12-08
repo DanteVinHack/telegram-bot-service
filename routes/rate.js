0e10ba21f05ea684b52ec97126cabc084dbc8c4e
DanteVinHack <ventilyatortag@gmail.com>
2022-12-08 11:18:19 +0100

    added handler put REST API request

--- routes/rate.js
+++ routes/rate.js
@@ -1,31 +1,30 @@
-const Rate = require('../models/Rate');
+const Rate = require("../models/Rate");
 
-const Router = require('express');
+const Router = require("express");
 const router = Router();
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id: _id } = req.params;
 
-  const rate = await Rate.findOne({_id});
+  const rate = await Rate.findOne({ _id });
 
-  res.status(200).json(rate)
-})
+  res.status(200).json(rate);
+});
 
-router.post('/', async (req, res) => {
+router.put("/", async (req, res) => {
   const rate = req.body;
 
-  await Rate.updateOne({_id: rate._id}, {$set: rate});
+  await Rate.updateOne({ _id: rate._id }, { $set: rate });
 
-  res.status(201).json('Update')
-})
+  res.status(201).json("Update");
+});
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
 
   await Rate.deleteOne({ _id: id });
 
-  res.status(200).json('Успешно удалено')
-})
+  res.status(200).json("Успешно удалено");
+});
 
-
-module.exports = {name: 'rate', router};
+module.exports = { name: "rate", router };
