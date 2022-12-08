f1b32459fe2a40936ada7ff3f5525a092bce5431
DanteVinHack <ventilyatortag@gmail.com>
2022-12-08 11:01:43 +0100

    change emit

--- client/src/components/UI/InputFile.vue
+++ client/src/components/UI/InputFile.vue
@@ -1,48 +1,22 @@
 <template>
-<input 
-  class="form-control" 
-  type="file"
-  @change='getFile'
->
+  <input class="form-control" type="file" accept="image/*" @change="getFile" />
 </template>
 
 <script>
-export default {
-  name: 'InputFile',
-  props: {
-    msg: {
-      typeof: String,
-      require: true
-    }
-  },
-  methods: {
-    getFile(e) {
-      const [ file ] = e.target.files;
-      const reader = new FileReader();
-
-      if (file.type.includes('image')) {
-
-        reader.readAsText(file);
-        
-        reader.onload = async () => {
-          try {
-            const { result } = reader;
-
-            this.$emit('get-file', result)
-            
-          } catch (error) {
-            console.log(error.message)
-          }
-        }
-        
-        reader.onerror = () => {
-          console.log('Возникла ошибка при прочении файла')
-        }
-        
-      } else {
-        console.log('Этот файл не является картинкой')
-      }
+  export default {
+    name: "InputFile",
+    props: {
+      msg: {
+        typeof: String,
+        require: true,
+      },
     },
-  }
-}
+    methods: {
+      getFile(e) {
+        const [file] = e.target.files;
+
+        this.$emit("get-image", file);
+      },
+    },
+  };
 </script>
