e8e070cfafe278abdb29c6acf30dfd3b5a31b73b
DanteVinHack <ventilyatortag@gmail.com>
2022-12-09 04:35:57 +0100

    change key startImage on startImagePath

--- models/Setting.js
+++ models/Setting.js
@@ -1,11 +1,11 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
   token: String,
   publicKey: String,
   secretKey: String,
-  startImage: String,
-  startText: String
-})
+  startImagePath: String,
+  startText: String,
+});
 
-module.exports = model('Setting', schema)
+module.exports = model("Setting", schema);
