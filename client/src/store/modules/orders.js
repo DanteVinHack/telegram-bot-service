4165e87bc84d2466f328c19b9d71c11df6c85717
DanteVinHack <ventilyatortag@gmail.com>
2022-12-09 21:26:04 +0100

    changed getOrders

--- client/src/store/modules/orders.js
+++ client/src/store/modules/orders.js
@@ -1,31 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   state: {
-    orders: []
+    orders: [],
   },
   getters: {
     orders(state) {
-      return state.orders;
+      return state.orders.reverse();
     },
   },
   mutations: {
     changeOrders(state, orders) {
       state.orders = orders;
-    }
+    },
   },
   actions: {
     async getOrders({ commit }) {
       try {
-        commit('toggleIsLoading')
-        const response = await axios.get('http://localhost:4000/orders');
+        commit("toggleIsLoading");
+        const response = await axios.get("http://localhost:4000/orders");
         const orders = await response.data;
-        commit('toggleIsLoading')
+        commit("toggleIsLoading");
 
-        commit('changeOrders', orders);
+        commit("changeOrders", orders);
       } catch (error) {
-          console.log(error.message);
+        console.log(error.message);
       }
-    }
+    },
   },
-}
+};
