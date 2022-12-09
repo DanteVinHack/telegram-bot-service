0290f1deeddeb1d8d5a3efda306cf6a458702537
DanteVinHack <ventilyatortag@gmail.com>
2022-12-09 21:25:22 +0100

    getUsers func move in it

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -1,28 +1,30 @@
 <template>
-<div class='main-layout h-100'>
-  <NavBar />
-  <div class="d-flex g-0 h-100">
-    <div class="col-md-2">
-      <SideBar 
-        class='bg-dark'
-      />
-    </div>
-    <div class="col-md-10 bg-secondary bg-opacity-25 overflow-auto">
-      <router-view class='m-3 mb-5 w-auto bg-light p-3'/>
+  <div class="main-layout h-100">
+    <NavBar />
+    <div class="d-flex g-0 h-100">
+      <div class="col-md-2">
+        <SideBar class="bg-dark" />
+      </div>
+      <div class="col-md-10 bg-secondary bg-opacity-25 overflow-auto">
+        <router-view class="m-3 mb-5 w-auto bg-light p-3" />
+      </div>
     </div>
   </div>
-
-</div>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar';
-import SideBar from '@/components/SideBar';
+import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'main-layout',
-  components: {NavBar, SideBar},
-}
+  name: "main-layout",
+  components: { NavBar, SideBar },
+  computed: mapGetters(["getUsers"]),
+  created() {
+    this.getUsers();
+  },
+};
 </script>
 
 <style>
