961af3abab288e0ee027c740481c653df6539257
DanteVinHack <ventilyatortag@gmail.com>
2022-12-11 20:44:33 +0100

    added set mongoose method

--- index.js
+++ index.js
@@ -38,6 +38,9 @@ const serverStart = async () => {
     connectionRouters(Routers);
 
     // Connection mongoose
+    mongoose.set({
+      strictQuery: true,
+    });
     await mongoose.connect(config.get("db_uri"));
     console.log("Database connected...");
 
